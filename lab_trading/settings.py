fc1120d469f349e8c36df4b1f2b65aab56f96e5f
Louis Dhanis <louis@fresheo.be>
2022-08-10 14:19:36 +0200

    eb update and set to eu-west-3

--- lab_trading/settings.py
+++ lab_trading/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u3d$$57wv76jda%*(vtg8#6ccq5%p(7u+2$jbsxw2osh3ckc&u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
